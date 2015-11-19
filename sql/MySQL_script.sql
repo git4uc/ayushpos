1b6c72355559b08d3e95af0040f6f88c94dbeada
git4uc <uchacko@yahoo.com>
2015-11-18 21:53:17 -0800

    wwith '#' will be ignored, and an empty message aborts the commit.ith '#' will be ignored, and an empty message aborts the commit. updated the sql :;# On branch master

--- sql/MySQL_script.sql
+++ sql/MySQL_script.sql
@@ -1,32 +1,87 @@
 create database usersdb;
-
+DROP TABLE IF EXISTS test.users;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
+  `PHONE` varchar(20) NOT NULL,
+  `FAX` varchar(20) DEFAULT NULL,
+  `ADD1` varchar(20) NOT NULL,
+  `ADD2` varchar(20) DEFAULT NULL,
+  `ADD3` varchar(20) DEFAULT NULL,
+  `CARDNO` varchar(20) DEFAULT NULL,
+  `CARDEXPDT` date DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS test.items;
-CREATE TABLE `items` (  `id` int(11) NOT NULL,
-`code` varchar(15) NOT NULL ,
-`category` varchar(15) NOT NULL ,
-`name` varchar(15) NOT NULL ,
-`price` double NOT NULL ,
-`stock` int NOT NULL ,
-`saleprice` double NOT NULL ,
-`tax` long NOT NULL ,
-`discount` long NOT NULL ,
-`brandname` varchar(15) NOT NULL ,
-`companyname` varchar(15) NOT NULL ,
-`catalog` varchar(15) NOT NULL ,
-`remarks` varchar(15) NOT NULL ,
-`updatedby` varchar(15) NOT NULL ,
-`updatedone` varchar(15) NOT NULL ,
-`discontinued` varchar(15) NOT NULL ,
-`reasondiscont` varchar(15) NOT NULL,
-PRIMARY KEY (`id`)
-)
+DROP TABLE IF EXISTS test.items;CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(15) DEFAULT NULL,
+  `category` varchar(15) DEFAULT NULL,
+  `name` varchar(15) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `saleprice` double DEFAULT NULL,
+  `tax` mediumtext,
+  `discount` mediumtext,
+  `brandname` varchar(15) DEFAULT NULL,
+  `catalog` varchar(15) DEFAULT NULL,
+  `remarks` varchar(15) DEFAULT NULL,
+  `updatedby` varchar(15) DEFAULT NULL,
+  `updatedone` varchar(15) DEFAULT NULL,
+  `discontinued` varchar(15) DEFAULT NULL,
+  `reasondiscont` varchar(15) DEFAULT NULL,
+  `image` blob,
+  `companyname` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
+DROP PROCEDURE IF EXISTS test.GetAllItems;
+CREATE PROCEDURE test.`GetAllItems`()
+BEGIN
+   SELECT * FROM items ;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetAllUsers;
+CREATE PROCEDURE test.`GetAllUsers`()
+BEGIN
+   SELECT * FROM users ;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetItemByCode;
+CREATE PROCEDURE test.`GetItemByCode`(cd varchar(20))
+BEGIN
+   SELECT * FROM items where code = cd;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetItemByID;
+CREATE PROCEDURE test.`GetItemByID`(itemid int)
+BEGIN
+   SELECT * FROM items where id = itemid;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetItemByName;
+CREATE PROCEDURE test.`GetItemByName`(itemname varchar(20))
+BEGIN
+   SELECT * FROM items where name LIKE CONCAT('%',itemname,'%');
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetStocks;
+CREATE PROCEDURE test.`GetStocks`(int_stockcode varchar(20))
+BEGIN
+   SELECT * FROM stock where stock_code = int_stockcode;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetUserByID;
+CREATE PROCEDURE test.`GetUserByID`(userid int)
+BEGIN
+   SELECT * FROM users where user_id = userid;
+   END;
+
+DROP PROCEDURE IF EXISTS test.GetUserByName;
+CREATE PROCEDURE test.`GetUserByName`(usrname varchar(20))
+BEGIN
+   SELECT * FROM users where username LIKE CONCAT('%',usrname,'%');
+   END;
 
